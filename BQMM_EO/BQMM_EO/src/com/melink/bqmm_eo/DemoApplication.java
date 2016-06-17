a9c27ca068eb8d7da94b0d83b12b656020be3998
xiaomaguohe007 <pyouqun2008@163.com>
2016-06-17 16:06:43 +0800

    替换jar包

--- BQMM_EO/BQMM_EO/src/com/melink/bqmm_eo/DemoApplication.java
+++ BQMM_EO/BQMM_EO/src/com/melink/bqmm_eo/DemoApplication.java
@@ -9,8 +9,7 @@ public class DemoApplication extends Application{
 	
 	@Override
 	public void onCreate() {
-		super.onCreate();
-		BQMM.getInstance().initConfig(getApplicationContext(),"925ee31c6dc649bcb7ddac5c0079c4ab", "662c0dd3d15b41bf86772670bbfa607d");	
-//		BQMM.getInstance().initConfig(getApplicationContext(),"YOUR_APPID", "YOUR_APPSECRET");	
+		super.onCreate();	
+		BQMM.getInstance().initConfig(getApplicationContext(),"YOUR_APPID", "YOUR_APPSECRET");	
 	}
 }
